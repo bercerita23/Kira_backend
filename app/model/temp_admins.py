fe66be5c859b582ae7b163fafe76b8dc7b8628fa
KhashayarMoradpour <khashayar@bercerita.org>
2025-06-30 12:37:26 -0700

    temp_admins model complete

---
+++ app/model/temp_admins.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database.base_class import Base
+
+class TempAdmin(Base):
+    __tablename__ = "temp_admins"
+    
+    user_id = Column(String(12), primary_key=True, index=True)
+    school_id = Column(String(8), ForeignKey("schools.school_id"), nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    first_name = Column(String(255), nullable=False)
+    last_name = Column(String(255), nullable=True)
+    verified = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.now())
+
+    school = relationship("School", back_populates="temp_admins")
