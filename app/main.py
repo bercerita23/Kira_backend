12daeafa688c7ae56f0cd73ba7c9bd84199c8106
Sean Hsieh <seankh4444@gmail.com>
2025-08-17 16:17:16 -0700

    Aug 17 temp disable background

--- app/main.py
+++ app/main.py
@@ -14,45 +14,45 @@ from app.router import (
 )
 from app.config import settings
 
-background_tasks = set()
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup logic
-    print("Starting up application...")
-    
-
-    # Start the background task
-    # hello_world_task = asyncio.create_task(hello_world())
-    # hello_sky_task = asyncio.create_task(hello_sky())
-    prompt_task = asyncio.create_task(prompt_generation())
-
-
-    ready_task = asyncio.create_task(ready_for_review())
-
-    # Add tasks to the set for tracking
-    # background_tasks.add(hello_world_task)
-    # background_tasks.add(hello_sky_task)
-    background_tasks.add(prompt_task)
-    background_tasks.add(ready_task)
-
-    # auto cleanup for taskss
-    # hello_world_task.add_done_callback(background_tasks.discard)
-    # hello_sky_task.add_done_callback(background_tasks.discard) 
-    prompt_task.add_done_callback(background_tasks.discard) 
-    prompt_task.add_done_callback(background_tasks.discard)
-    yield
-    
-    # Shutdown logic
-    print("Shutting down application...")
-    
-    # Cancel all background tasks
-    for task in background_tasks:
-        task.cancel()
-    
-    # Wait for all tasks to complete cancellation
-    if background_tasks:
-        await asyncio.gather(*background_tasks, return_exceptions=True)
+#background_tasks = set()
+#
+#@asynccontextmanager
+#async def lifespan(app: FastAPI):
+#    # Startup logic
+#    print("Starting up application...")
+#    
+#
+#    # Start the background task
+#    # hello_world_task = asyncio.create_task(hello_world())
+#    # hello_sky_task = asyncio.create_task(hello_sky())
+#    prompt_task = asyncio.create_task(prompt_generation())
+#
+#
+#    ready_task = asyncio.create_task(ready_for_review())
+#
+#    # Add tasks to the set for tracking
+#    # background_tasks.add(hello_world_task)
+#    # background_tasks.add(hello_sky_task)
+#    background_tasks.add(prompt_task)
+#    background_tasks.add(ready_task)
+#
+#    # auto cleanup for taskss
+#    # hello_world_task.add_done_callback(background_tasks.discard)
+#    # hello_sky_task.add_done_callback(background_tasks.discard) 
+#    prompt_task.add_done_callback(background_tasks.discard) 
+#    prompt_task.add_done_callback(background_tasks.discard)
+#    yield
+#    
+#    # Shutdown logic
+#    print("Shutting down application...")
+#    
+#    # Cancel all background tasks
+#    for task in background_tasks:
+#        task.cancel()
+#    
+#    # Wait for all tasks to complete cancellation
+#    if background_tasks:
+#        await asyncio.gather(*background_tasks, return_exceptions=True)
 
 
 app = FastAPI(lifespan=lifespan, 
