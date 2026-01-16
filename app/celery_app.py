f1fae2e1e7e61f7d500a93caeb8db64b906be210
Sai <mr.saiwong@gmail.com>
2026-01-15 16:15:31 -0800

    Aws redis-celery workaround

--- app/celery_app.py
+++ app/celery_app.py
@@ -2,7 +2,6 @@ from celery import Celery
 from app.config import settings
 from celery.schedules import crontab
 
-
 celery_app = Celery(
     "kira_backend",
     broker=settings.CELERY_BROKER_URL,
@@ -17,7 +16,14 @@ celery_app.conf.update(
     result_serializer="json",
     timezone="UTC",
     enable_utc=True,
-)
+    broker_transport_options={
+        "global_keyprefix": "{queue}:", 
+        "fanout_prefix": True,
+        "fanout_patterns": True,
+    },
+    worker_send_task_events=False,  
+    worker_enable_remote_control=False,  
+    worker_pool_restarts=True)
 
 celery_app.conf.beat_schedule = {
     'bq_upsert': {
